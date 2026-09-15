56ff17c0e53b6b356118f5d611371ef02f805cbf
eveleighoj <owen.eveleigh@communities.gov.uk>
2026-09-15 16:35:35 +0100

    fix url paths

--- eleventy.config.js
+++ eleventy.config.js
@@ -23,7 +23,7 @@ export default function (eleventyConfig) {
     },
     url:
       process.env.GITHUB_ACTIONS &&
-      'https://digital-land.github.io/data-design-design-history/'
+      'https://digital-land.github.io/consumers-design-history/'
   })
 
   // Passthrough
@@ -49,6 +49,6 @@ export default function (eleventyConfig) {
       layouts: '_layouts',
       includes: '_components'
     },
-    pathPrefix: process.env.GITHUB_ACTIONS && '/data-design-design-history/'
+    pathPrefix: process.env.GITHUB_ACTIONS && '/consumers-design-history/'
   }
 }
